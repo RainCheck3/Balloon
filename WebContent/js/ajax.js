004759e8a3e544f35136c1b1e64701e36a27a4e5
LsCodeMaster <i.liszewski@comcast.net>
2015-10-09 14:29:00 +0530

    form finished

--- WebContent/js/ajax.js
+++ WebContent/js/ajax.js
@@ -1,23 +1,12 @@
 $(document).ready(function() { 
 	$('#updateBtn').click(function(event) {
 	        // get the form data
-	        // there are many ways to get this data using jQuery (you can use the class or id also)
-	        var formData = {
-	            'fName'             : $("input[name='fName']").val(),
-	            'lName'             : $("input[name='lName']").val(),
-	            'email'    			: $("input[name='email']").val(),
-	            'street'			: $("input[name='street'").val(),
-	            'city'				: $("input[name='city'").val(),
-	            'state'				: $("input[name='state'").val(),
-	            'zip'				: $("input[name='zip'").val(),
-	            'country'			: $("input[name='country'").val()
-	        };
-	
+	        // there are many ways to get this data using jQuery (you can use the class or id also)	
 	        // process the form
 	        $.ajax({
 	            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-	            url         : 'rest/account/upd', // the url where we want to POST
-	            data        : formData, // our data object
+	            url         : 'acnt', // the url where we want to POST
+	            data        : $('#updateCustomer').serialize(), // our data object
 	            success		: function(result){
 	            	$("#updateResponse").html("Account Info Updated");
 	            	$("#hello").html("<p>Hello " + result + "</p>");
--- src/com/sapient/controller/NavigationCtrl.java
+++ src/com/sapient/controller/NavigationCtrl.java
@@ -1,12 +1,28 @@
 package com.sapient.controller;
 
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.hibernate.mapping.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sapient.dao.BalloonDao;
+import com.sapient.dao.BalloonDaoImpl;
+import com.sapient.model.customer.NewCustomer;
+import com.sapient.model.customer.UpdateCustomer;
 
 
 @Controller
 public class NavigationCtrl {
+	Logger log;
 	
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String homePage() {
@@ -29,14 +45,36 @@ public class NavigationCtrl {
 	}
 	
 	@RequestMapping(value = "/acnt", method = RequestMethod.GET)
-	public String account() {
+	public String account(ModelMap model, HttpSession session ) {
+		log = Logger.getLogger(NavigationCtrl.class.getName());
+		BasicConfigurator.configure();
+		BalloonDao dao = new BalloonDaoImpl();
+		int customerId = (Integer)dao.getCustomerId("Gunther");
+		UpdateCustomer customer = dao.getUser(customerId);
+		model.addAttribute("customer", customer);
 		return "MyAccount";
 	}
 	
 	@RequestMapping(value = "/acnt", method = RequestMethod.POST)
-	public String updateAccount() {
-		return "MyAccount";
+	public String updateAccount(@ModelAttribute("customer") @Valid UpdateCustomer customer, BindingResult result) {
+//		log.info("first name: " + request.getParameter("fName"));
+//		log.info("last name: " + request.getParameter("lName"));
+		log = Logger.getLogger(NavigationCtrl.class.getName());
+		BasicConfigurator.configure();
+		log.info("made it to post func");
+		if(result.hasErrors())
+		{
+			log.info(result.getAllErrors());
+			return "MyAccount";
+		}
+		else
+		{
+			log.info("no errors");
+			BalloonDao updateDB = new BalloonDaoImpl();
+			updateDB.updateUser(customer,10);
+//			request.getSession().setAttribute("name", fName);
+			return "MyAccount";
+		}
 	}
-	
-	
+
 }
